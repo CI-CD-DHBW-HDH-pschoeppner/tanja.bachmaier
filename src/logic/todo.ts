4829896819c8381f690493f9e3168d7181a3a1bc
Tany06 <115782073+Tany06@users.noreply.github.com>
2023-03-21 09:48:33 +0100

    todo in todo.ts done + install lint jest prettier

--- src/logic/todo.ts
+++ src/logic/todo.ts
@@ -1,4 +1,5 @@
 import { writable } from "svelte/store";
+import { v4 as uuidv4 } from "uuid";
 
 export class TodoItem {
   id = "";
@@ -8,8 +9,7 @@ export class TodoItem {
 
 // this function must return a unique ID every time it is called
 export function generateID(): string {
-    // TODO: implement
-    return ""
+  return uuidv4();
 }
 
 // make sure, that
@@ -17,25 +17,45 @@ export function generateID(): string {
 // the value isn' empty
 // the todo isn't contained in the todos array (case insensitive)
 export function validateTodo(todo: TodoItem, todos: TodoItem[]): boolean {
-  // TODO: implement
-  return false;
+  const maxChar = 255;
+
+  if (!todo.value.trim() || todo.value.length > maxChar) {
+    return false;
+  }
+
+  // Check if todo is contained in todos array (case-insensitive)
+  const todoValue = todo.value.toLowerCase();
+  const isDuplicate = todos.some(
+    (existingTodo) => existingTodo.value.toLowerCase() === todoValue);
+  if (isDuplicate) {
+    return false;
+  }
+
+  // If all validation checks pass, return true
+  return true;
 }
 
 // capitalize the first letter of the todo
 export function formatTodo(todo: TodoItem): TodoItem {
-  // TODO: implement
+  //get the value of the todo and set the first letter (index 0) to upper case
+  const capitalizedValue = todo.value.charAt(0).toUpperCase() + todo.value.slice(1)
   return {
     id: "",
-    value: "",
+    value: capitalizedValue,
     done: false
   }
 }
 
 // generate a random rgb color
-// each value (r,g,b) should be between 50 and 150
+// each value (r,g,b) is between 50 and 150
 export function generateColor(): string {
-  // TODO: implement
-  return ""
+  const min = 50;
+  const max = 150;
+  //three random values for red, green, blue
+  const r = Math.floor(Math.random() * (max - min + 1) + min);
+  const g = Math.floor(Math.random() * (max - min + 1) + min);
+  const b = Math.floor(Math.random() * (max - min + 1) + min);
+  return `rgb(${r}, ${g}, ${b})`;
 }
 
 export const todoList = writable<TodoItem[]>([]);
